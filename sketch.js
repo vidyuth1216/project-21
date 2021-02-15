484a97aad4e41bb332fb911d978b0d6bd47880fa
vidyuth1216 <75615784+vidyuth1216@users.noreply.github.com>
2021-02-15 11:40:55 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var wall;
+var thickness;
+var bullet;
+var speed;
+var weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  speed = random(223,321);
+  weight = random(30,52 );
+  thickness = random(22,83);
+
+  bullet = createSprite(50,200,50,50);
+  bullet.shapeColor = "red"
+  wall = createSprite(1200,200,thickness,height/2);
+  wall.shapeColor = color(80,80,80);
+
+  bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background(0,0,0);  
+ 
+  if(hasCollied(bullet,wall)){
+
+      bullet.velocityX = 0;
+      var damege = 0.5 * speed * speed/(thickness* thickness * thickness);
+
+      if(damege > 10)
+      {
+        wall.shapeColor = color(255,0,0);
+      }
+
+      if(damege < 10)
+      {
+        wall.shapeColor = color(0,255,0);
+      }
+  }
+
+  drawSprites();
+}
+
+function hasCollied(lbullet,lwall){
+
+  console.log(lbullet);
+  console.log(lwall);
+  bulletRightEdge = lbullet.x + lbullet.width;
+  wallLeftEdge = lwall.x;
+  if(bulletRightEdge >= wallLeftEdge)
+  {
+    return true;
+  }
+  return false;
+}
